af312a5828ab2550f83a920a57903ecf23f1463f
raynecheong <s10267916@connect.np.edu.sg>
2025-01-14 21:02:15 +0800

    airline class

---
+++ PRG2-T13-06/Airline.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T13_06
+{
+    internal class Airline
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Airline() { }
+        public Airline(string name, string code)
+        {
+            Name = name;
+            Code = code;
+            Flights = new Dictionary<string, Flight>();
+        }
+
+        public bool AddFlight(Flight flight)
+        {
+            if (!Flights.ContainsKey(flight.FlightNumber))
+            {
+                Flights.Add(flight.FlightNumber, flight);
+                return true;
+            }
+            return false;
+        }
+        public bool RemoveFlight(Flight flight) { return Flights.Remove(flight.FlightNumber); }
+
+        public double CalculateFees()
+        {
+            double totalFees = 0;
+            foreach (var flight in Flights.Values)
+            {
+                totalFees += flight.CalculateFees();
+            }
+            return totalFees;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Code}) has {Flights.Count} flight(s)";
+        }
+    }
+}
