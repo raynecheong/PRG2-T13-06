95913011c83e2a896892e2eea837654f71bf2d17
raynecheong <s10267916@connect.np.edu.sg>
2025-01-15 13:54:36 +0800

    Terminal class

---
+++ PRG2-T13-06/Terminal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T13_06
+{
+    internal class Terminal
+    {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+        public Dictionary<string, double> GateFees { get; set; }
+        public Terminal() { }
+        public Terminal(string terminalName)
+        {
+            TerminalName = terminalName;
+            Airlines = new Dictionary<string, Airline>();
+            Flights = new Dictionary<string, Flight>();
+            BoardingGates = new Dictionary<string, BoardingGate>();
+            GateFees = new Dictionary<string, double>();
+        }
+
+        public bool AddAirline(Airline airline)
+        {
+            if (!Airlines.ContainsKey(airline.Code))
+            {
+                Airlines.Add(airline.Code, airline);
+                return true;
+            }
+            return false;
+        }
+
+        public bool AddBoardingGate(BoardingGate boardinggate)
+        {
+            if (!BoardingGates.ContainsKey(boardinggate.GateName))
+            {
+                BoardingGates.Add(boardinggate.GateName, boardinggate);
+                return true;
+            }
+            return false;
+        }
+
+        public Airline GetAirlineFromFlight(Flight flight)
+        {
+            foreach (var airline in Airlines.Values)
+            {
+                if (airline.Flights.ContainsKey(flight.FlightNumber))
+                {
+                    return airline;
+                }
+            }
+            return null;
+        }
+
+        public void PrintAirlineFees()
+        {
+            foreach (var airline in Airlines.Values)
+            {
+                double fees = airline.CalculateFees();
+                Console.WriteLine($"Airline {airline.Name} {airline.Code} Fees: {fees:C2}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Terminal: {TerminalName}, Airlines: {Airlines.Count}, Boarding Gates: {BoardingGates.Count}";
+        }
+    }
+}
